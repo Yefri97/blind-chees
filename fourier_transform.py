1c50b63cce1ba5036b8484e89ac6a33ccb0f5c20
Yeferson Gaitan <yefri.gaitan97@gmail.com>
2017-11-26 02:02:57 +0000

    Implemention of the FFT and DFT finished

--- fourier_transform.py
+++ fourier_transform.py
@@ -1,13 +1,40 @@
+# --------------------------------------------------------------
+# blind chees: fourier_transform.py
+# --------------------------------------------------------------
+
 import numpy as np
 
-def dft(x):
-    N = x.shape[0]
+def dft(s):
+    """
+    Get the Discrete Fourier Transform from the input signal.
+    Time complexity: O(n ^ 2)
+
+    Arguments:
+    s -- A numpy array representing the signal.
+
+    Return:
+    S -- A numpy array with the dft from the signal.
+    """
+    N = s.shape[0]
     n = np.arange(N)
     k = n.reshape((N, 1))
     M = np.exp(-2j * np.pi * k * n / N)
-    return np.dot(M, x)
+    S = np.dot(M, x)
+    return S
 
-def fft(x):
+def fft(s):
+    """
+    Get the Discrete Fourier Transform using the fast
+    Cooley - Tukey Algorithm from the input signal.
+    Time complexity: O(n * log(n))
+    note: n has to be a power of two
+
+    Arguments:
+    s -- A numpy array representing the signal.
+
+    Return:
+    S -- A numpy array with the fft from the signal.
+    """
     n = x.shape[0]
     if n == 1:
         return x
@@ -18,10 +45,3 @@ def fft(x):
     y1 = x_even + wi[n//2:] * x_odd
     y = np.concatenate((y0, y1))
     return y
-
-def dct(x):
-    N = x.shape[0]
-    n = np.arange(N)
-    k = n.reshape((N, 1))
-    M = np.cos(-2 * np.pi * k * n / N)
-    return np.dot(M, x)
